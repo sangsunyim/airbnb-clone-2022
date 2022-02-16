92386ec8b9e5f93b4e91b97ac289a852598a97c3
sangsun yim <yimss3842@gmail.com>
2022-02-16 11:27:35 +0900

    Firsttime Setting Admin Panel

--- users/models.py
+++ users/models.py
@@ -6,14 +6,30 @@ class User(AbstractUser):
 
     """ Custom User Model """
 
-    GENDER_MALE = "Male"
-    GENDER_FEMALE = "Female"
-    GENDER_OTHER = "Other"
+    GENDER_MALE = "male"
+    GENDER_FEMALE = "female"
+    GENDER_OTHER = "other"
 
     GENDER_CHOICE = (
         (GENDER_MALE, "Male"),
-        (GENDER_MALE, "Female"),
-        (GENDER_MALE, "Other"),
+        (GENDER_FEMALE, "Female"),
+        (GENDER_OTHER, "Other"),
+    )
+
+    LANGUAGE_ENGLISH = "en"
+    LANGUAGE_KOREAN = "kR"
+
+    LANGUAGE_CHOICE = (
+        (LANGUAGE_ENGLISH, "English"),
+        (LANGUAGE_KOREAN, "Korean"),
+    )
+
+    CURRENCY_USD = "usd"
+    CURRENCY_KRW = "krw"
+
+    CURRENCY_CHOICE = (
+        (CURRENCY_USD, "USD"),
+        (CURRENCY_KRW, "KRW"),
     )
 
     avatar = models.ImageField(null=True, blank=True)
@@ -21,3 +37,11 @@ class User(AbstractUser):
         choices=GENDER_CHOICE, max_length=10, null=True, blank=True
     )
     bio = models.TextField(default="", blank=True)
+    birthdate = models.DateField(null=True)
+    language = models.CharField(
+        choices=LANGUAGE_CHOICE, max_length=2, null=True, blank=True
+    )
+    currency = models.CharField(
+        choices=CURRENCY_CHOICE, max_length=3, null=True, blank=True
+    )
+    superhost = models.BooleanField(default=False)
