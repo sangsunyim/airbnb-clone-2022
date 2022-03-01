3a5470bd1bec8e1576aae8d32ecc2bf8e1321d40
sangsun yim <yimss3842@gmail.com>
2022-03-01 12:28:50 +0900

    About Templates

--- resevations/management/commands/seed_reservations.py
+++ resevations/management/commands/seed_reservations.py
@@ -2,7 +2,7 @@ import random
 from datetime import datetime, timedelta
 from django.core.management.base import BaseCommand
 from django_seed import Seed
-from reservations import models as resevation_models
+from resevations import models as resevation_models
 from users import models as user_models
 from rooms import models as room_models
 
