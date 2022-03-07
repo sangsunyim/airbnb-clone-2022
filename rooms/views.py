f4c216f7ffab3b645a22701dbe0c664347151569
sangsun yim <yimss3842@gmail.com>
2022-03-07 20:48:40 +0900

    Detailview FBV

--- rooms/views.py
+++ rooms/views.py
@@ -1,4 +1,5 @@
 from django.views.generic import ListView
+from django.http import Http404
 from django.shortcuts import render
 from . import models
 
@@ -15,5 +16,8 @@ class HomeView(ListView):
 
 
 def room_detail(request, pk):
-    print(pk)
-    return render(request, "rooms/detail.html")
+    try:
+        room = models.Room.objects.get(pk=pk)
+        return render(request, "rooms/detail.html", {"room": room})
+    except models.Room.DoesNotExist:
+        raise Http404()
